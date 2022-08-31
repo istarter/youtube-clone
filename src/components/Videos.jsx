b2b93e40d99adbaa4d9377a73f9e453842a29b23
Najib Khan <najibkhan@192.168.1.5>
2022-08-31 22:31:58 +0500

    VideoCard, ChannelDeatil created

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,24 @@
+import { Box, Stack } from "@mui/system";
 import React from "react";
+import { VideoCard, ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
-  return console.log(videos);
+  return (
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
